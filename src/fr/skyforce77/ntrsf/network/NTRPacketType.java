0c15db629ef24727f022dedca3d9985163508de2
Skyforce77 <arnaud.bruyer@gmail.com>
2016-11-26 22:52:06 +0100

    Added packets

--- src/fr/skyforce77/ntrsf/network/NTRPacketType.java
+++ src/fr/skyforce77/ntrsf/network/NTRPacketType.java
@@ -5,7 +5,12 @@ public enum NTRPacketType {
 	HEARTBEAT(0, 0),
 	HELLO(0, 3),
 	RELOAD(0, 4),
+	LIST_PROCESSES(0, 5),
+	ATTACH_PROCESS(0, 6), // Args: PID, patchAddr (0)
+	LIST_THREADS(0, 7), // Args: PID
+	MEM_LAYOUT(0, 8), // Args: PID
 	READ_MEMORY(0, 9),
+	QUERY_HANDLE(0, 12), // Args: PID
 	
 	SAVE_FILE(1, 1),
 	WRITE_MEMORY(1, 10),
