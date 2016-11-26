836170069e05ac2d3b20aa3dd9e9f9547fcb2535
Skyforce77 <arnaud.bruyer@gmail.com>
2016-11-26 23:07:15 +0100

    heartbeat charset

--- src/fr/skyforce77/ntrsf/network/InternalPacketListener.java
+++ src/fr/skyforce77/ntrsf/network/InternalPacketListener.java
@@ -1,12 +1,14 @@
 package fr.skyforce77.ntrsf.network;
 
+import java.nio.charset.Charset;
+
 class InternalPacketListener implements NTRPacketListener {
 
 	@Override
 	public void onPacketReceived(NTRPacket packet) {
 		if(packet.getPacketType() == NTRPacketType.HEARTBEAT) {
 			if(packet.getData().length != 0)
-				System.out.println(new String(packet.getData()));
+				System.out.println(new String(packet.getData(), Charset.forName("UTF-8")));
 		}
 	}
 
